9cee84b0b6629d92b4c353ed9cae71fcaf5c4c55
Nguyen Thien Hung <ngthienhung277@gmail.com>
2024-08-15 20:13:12 +0700

    first commit

---
+++ src/Drum.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { AudioClip } from "./type";
+
+interface DrumProps {
+  audioClip: AudioClip;
+}
+const Drum = ({ audioClip }: DrumProps) => {
+  const playSound = (clip: AudioClip) => {
+    (document.getElementById(clip.keyTrigger) as HTMLAudioElement)
+      .play()
+      .catch(console.error);
+      document.getElementById("display")!.textContent = clip.description;
+  };
+
+  return (
+    <button
+      className="drum-pad"
+      id={`drum ${audioClip.keyTrigger}`}
+      onClick={() => playSound(audioClip)}
+    >
+      <audio src={audioClip.url} id={audioClip.keyTrigger} className="clip" />
+      {audioClip.keyTrigger}
+    </button>
+  );
+};
+
+export default Drum;
